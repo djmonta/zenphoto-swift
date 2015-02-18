485b80fe824d3019fcc399067e897ceeb77e0c93
monta <djmonta@gmail.com>
2015-02-18 19:10:01 +0900

    Initial Commit

---
+++ zenphoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  zenphoto
+//
+//  Created by 宮本幸子 on 2015/02/18.
+//  Copyright (c) 2015年 宮本幸子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
