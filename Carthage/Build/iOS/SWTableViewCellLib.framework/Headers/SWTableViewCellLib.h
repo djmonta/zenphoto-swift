854b9f7b604632a07e451e422ec11849082772f7
monta <djmonta@gmail.com>
2015-08-06 03:45:02 +0900

    Add SWTableViewCellLib

---
+++ Carthage/Build/iOS/SWTableViewCellLib.framework/Headers/SWTableViewCellLib.h
@@ -0,0 +1,19 @@
+//
+//  SWTableViewCellLib.h
+//  SWTableViewCellLib
+//
+//  Created by 宮本幸子 on 2015/08/06.
+//  Copyright (c) 2015年 Chris Wendel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWTableViewCellLib.
+FOUNDATION_EXPORT double SWTableViewCellLibVersionNumber;
+
+//! Project version string for SWTableViewCellLib.
+FOUNDATION_EXPORT const unsigned char SWTableViewCellLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWTableViewCellLib/PublicHeader.h>
+
+
